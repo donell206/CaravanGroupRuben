87e0554b5ca3509da3f1b831148755d36469cbf4
rubenspbn <rubenspillebeen@gmail.com>
2020-12-11 11:45:40 +0100

    Add project files.

---
+++ CaravanLib/Models/Boot.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaravanLib.Models
+{
+    public class Boot
+    {
+        #region PROPERTIES
+
+        #endregion
+        #region CONSTRUCTORS
+
+        #endregion
+        #region METHODS
+        /// <summary>
+        /// makes a correct representation of the class
+        /// </summary>
+        /// <returns>formatted class</returns>
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+        #endregion
+    }
+}
